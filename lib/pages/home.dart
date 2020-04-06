00eca4ae2df2786f124525ef5e8a51bda3b353e3
LaupWing <loc_strict@hotmail.com>
2020-04-06 21:15:43 +0200

    video 23 done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,7 +9,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home screen'))
+      body: SafeArea(
+          child: Column(
+              children: <Widget>[
+                  FlatButton.icon(
+                    icon: Icon(Icons.edit_location),
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    label: Text('Edit Location'),
+                  )
+              ],
+            )
+        )
     );
   }
 }
