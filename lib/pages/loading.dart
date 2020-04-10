962ba1e7e32def78b673025c7d12a28f4e3ae299
LaupWing <loc_strict@hotmail.com>
2020-04-10 21:16:58 +0200

    episode 28

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +9,25 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+
+
+  void getTime() async {
+    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/Amsterdam');
+    Map data = jsonDecode(response.body);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getTime();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
