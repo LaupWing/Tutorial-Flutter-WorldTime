51936eb00b2e896616c360b6c9a5ef78b57d30b9
LaupWing <loc_strict@hotmail.com>
2020-04-11 18:49:26 +0200

    added custom class and error cathcing:

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:world_time/services/word_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -11,27 +9,29 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
 
-
-  void getTime() async {
-    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/Amsterdam');
-    Map data = jsonDecode(response.body);
-
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  String time = 'loading';
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Berlin', flag:'germany.png', endpoint: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
+
 
   @override
   void initState(){
     super.initState();
-    getTime();
+    setupWorldTime();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading Screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/word_time.dart
@@ -0,0 +1,30 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime{
+
+  String location;
+  String time;
+  String flag;
+  String endpoint;
+  WorldTime({this.location, this.flag, this.endpoint});
+  Future<void> getTime() async {
+    try{
+      Response response = await get('http://worldtimeapi.org/api/timezone/$endpoint');
+      Map data = jsonDecode(response.body);
+
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      time = now.toString();
+    }catch(e){
+      print('There is an error $e');
+      time = 'Could not get time data';
+    }
+
+  }
+
+}
+
